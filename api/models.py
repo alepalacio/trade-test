123c26c323f04dff98547eba6afbcadf01d69d2f
alepalacio <alejandropalacio24@gmail.com>
2022-01-19 23:52:59 -0300

    first commit, basic registry working

---
+++ api/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from api.managers import UserManager
+
+# Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    password = models.CharField(max_length=100)
+    # first_name = models.CharField(max_length=100, default="", blank=True)
+    # last_name = models.CharField(max_length=100, default="", blank=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    is_verified = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "username"
+    EMAIL_FIELD = "email"
+    REQUIRED_FIELDS = ['email']
+
+    def __str__(self):
+        return self.username
---
+++ api/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from rest_framework import status
+from api.serializers import UserSerializer
+
+class UserAPIView(APIView):
+    def post(self, request):
+        try:
+            serializer =  UserSerializer(data = request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        except:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
