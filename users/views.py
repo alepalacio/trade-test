1d53e828181541c777114fb4ff6c17629592387b
alepalacio <g17ada03@hackademy.mx>
2022-02-11 03:22:54 -0300

    modified token time expiracy on registry view to access token and modified api_key middleware

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 import jwt
+from datetime import timedelta
 from django.urls import reverse
 from django.contrib.sites.shortcuts import get_current_site
 from rest_framework.response import Response
@@ -7,7 +8,7 @@ from rest_framework import status
 from rest_framework import permissions
 from rest_framework_simplejwt.tokens import RefreshToken
 from middlewares.api_keys import check_api_key_client
-from users.serializers import UserSerializer, LoginSerializer
+from users.serializers import UserSerializer, LoginSerializer, LogoutSerializer
 from users.models import User
 from users.utils import EmailUtil
 from app import settings
@@ -25,35 +26,39 @@ class UserRegistryView(APIView):
         
     def post(self, request):
 
-        try:
-            check_api_key_client(self)
-            serializer =  UserSerializer(data = request.data)
-            serializer.is_valid(raise_exception=True)
-            serializer.save()
-            user_data = serializer.data
-            user = User.objects.get(email=user_data['email'])
-            token = RefreshToken.for_user(user).access_token
+        api_key = check_api_key_client(self)
 
-            current_site = get_current_site(request).domain
+        if api_key:
 
-            relative_url = reverse('email-verification')
+            try:
+                serializer =  UserSerializer(data = request.data)
+                serializer.is_valid(raise_exception=True)
+                serializer.save()
+                user_data = serializer.data
+                user = User.objects.get(email=user_data['email'])
+                token = RefreshToken.for_user(user).access_token
+                token.set_exp(lifetime=timedelta(days=1))
 
-            absolute_url = f"http://{current_site}{relative_url}?t={str(token)}"
+                current_site = get_current_site(request).domain
 
-            email_body = f"Hi, {user.username}. Please use link below to verify your email account. \n\n{absolute_url}"
+                relative_url = reverse('email-verification')
 
-            data = {
-                'subject':'Verify your email.',
-                'body':email_body,
-                'to_email':user.email
+                absolute_url = f"http://{current_site}{relative_url}?t={str(token)}"
 
-            }
+                email_body = f"Hi, {user.username}. Please use link below to verify your email account. \n\n{absolute_url}"
 
-            EmailUtil.send_email(data)
+                data = {
+                    'subject':'Verify your email.',
+                    'body':email_body,
+                    'to_email':user.email
 
-            return Response('Account successfully created.', status=status.HTTP_201_CREATED)
-        except:
-            return Response('Bad request.', status=status.HTTP_400_BAD_REQUEST)
+                }
+
+                EmailUtil.send_email(data)
+
+                return Response('Account successfully created.', status=status.HTTP_201_CREATED)
+            except:
+                return Response('Bad request.', status=status.HTTP_400_BAD_REQUEST)
 
 
 class VerifyEmailView(APIView):
@@ -121,17 +126,48 @@ class UserLoginView(APIView):
     permission_classes = [permissions.AllowAny]
     
     def post(self, request):
-        try:
-            check_api_key_client(self)
-            serializer = LoginSerializer(data = request.data)
-            serializer.is_valid(raise_exception=True)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        except:
-            return Response('Bad request.', status=status.HTTP_400_BAD_REQUEST)
+
+        api_key = check_api_key_client(self)
+
+        if api_key:
+            try:
+                serializer = LoginSerializer(data = request.data)
+                serializer.is_valid(raise_exception=True)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            except:
+                return Response('Bad request.', status=status.HTTP_400_BAD_REQUEST)
+
+class UserLogoutView(APIView):
+    """
+    User logout.  Input example:
+    {
+        "refresh":"mysupertoken$%&@"
+    }
+    """
+
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request):
+
+        api_key = check_api_key_client(self)
+
+        if api_key:
+            try:
+                serializer = LogoutSerializer(data=request.data)
+                serializer.is_valid(raise_exception=True)
+                serializer.save()
+                return Response('Succesfully logged out.', status=status.HTTP_200_OK)
+            except:
+                return Response('Bad request.', status=status.HTTP_400_BAD_REQUEST)
+        
+
+
 
 class TestView(APIView):
     permissions_classes = permissions.IsAuthenticated
 
     def get(self, request):
-        print(request.user.id)
+        print("request.user",request.user)
+        print("username", request.user.username)
+        print("email", request.user.email)
         return Response('Hola')
