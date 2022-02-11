ee1f49c8da5a9bf0e1c1b1418a0afe91028b899f
alepalacio <g17ada03@hackademy.mx>
2022-02-11 17:26:54 -0300

    modified api key client custom middleware

--- middlewares/api_keys.py
+++ middlewares/api_keys.py
@@ -1,13 +1,21 @@
+from django.forms import ValidationError
 from app import settings
 from rest_framework import exceptions
+from rest_framework.response import Response
+from rest_framework import status
 
 # Method to check incoming api key clients from frontend.  If exists and it's correct, allow functionality.
 
 def check_api_key_client(self):
     api_key = settings.API_CLIENT
-    front_api_key = self.request.META['HTTP_API_KEY']
 
-    if api_key != front_api_key:
-        raise exceptions.ValidationError('Missing or invalid API Key.')
-
-    return front_api_key
+    try:
+        front_api_key = self.request.META.get('HTTP_API_KEY', None)
+        if front_api_key is None:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        if api_key != front_api_key:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        if front_api_key:
+            return Response(status=status.HTTP_200_OK)
+    except:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
